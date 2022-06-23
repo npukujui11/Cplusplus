655566fb3e4090bb9445e2f4bd3703b2379d2465
npukujui11 <npukujui11@gmail.com>
2022-06-23 23:04:32 +0800

    auto的用法，可以用于变量和函数

--- program3.6.cpp
+++ program3.6.cpp
@@ -3,6 +3,14 @@ using namespace std;
 
 /*使用关键字auto依靠编译器的类型推断功能*/
 /*编译器只要支持C++11或者更高版本，auto可不显式的指定变量的类型*/
+/*从C++14起，auto亦可用于函数*/
+
+const double Pi = 3.1415926;
+
+auto Area(double radius)
+{
+    return Pi * radius * radius;
+}//编译器将根据return语句中使用double变量的表达式来推断返回类型
 
 int main(){
     auto coinFlippedHeads = true;
@@ -12,6 +20,12 @@ int main(){
     cout << " , sizeof(coinFlippedHeads) = " << sizeof(coinFlippedHeads) << endl;
     cout << "largeNumber = " << largeNumber;
     cout << " , sizeof(largeNumber) = " << sizeof(largeNumber) << endl;
+
+    cout << "Enter radius: ";
+    double radius = 0;
+    cin >> radius;
+
+    cout << "Area is: " << Area(radius) << endl;
 
     return 0;
 }
